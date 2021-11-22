8f6f2e6ac2ee46e7bfcd49546fb7e6f10ab6296d
duhone <duhone@outlook.com>
2021-11-21 18:50:35 -0800

    add catch3

---
+++ 3rdParty/include/3rdParty/catch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#pragma warning(push, 1)
+
+#include "catch/catch_amalgamated.hpp"
+
+#pragma warning(pop)
---
+++ common.cmake
@@ -0,0 +1,89 @@
+set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+set(CMAKE_CXX_STANDARD 20)
+
+if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
+    message(FATAL_ERROR "Do not build in-source.")
+endif()
+
+# WORKAROUND
+# This is added so that CMake can recognize the .ixx extension as a module interface.
+set(CMAKE_CXX_SYSROOT_FLAG_CODE "list(APPEND CMAKE_CXX_SOURCE_FILE_EXTENSIONS ixx)")
+
+# set(CMAKE_CXX_CLANG_TIDY clang-tidy -checks=cppcoreguidelines-*)
+
+function(addCommon target)	
+	target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/GR->)
+	
+	target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/arch:AVX>)
+	target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/fp:fast>)
+	target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/fp:except->)
+	target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/MP>)
+
+	target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:Clang>:-ffast-math>)
+
+	target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:GNU>:-ffast-math>)
+		
+	target_compile_options(${target} PRIVATE $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Profile>,$<CONFIG:Final>>>:/O2>)
+	target_compile_options(${target} PRIVATE $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Profile>,$<CONFIG:Final>>>:/Oi>)
+	target_compile_options(${target} PRIVATE $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Profile>,$<CONFIG:Final>>>:/Ot>)
+	target_compile_options(${target} PRIVATE $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Profile>,$<CONFIG:Final>>>:/Ob2>)
+	target_compile_options(${target} PRIVATE /sdl-)
+		
+	target_compile_definitions(${target} PRIVATE $<$<CONFIG:Debug>:CR_DEBUG=1>)
+	target_compile_definitions(${target} PRIVATE $<$<NOT:$<CONFIG:Debug>>:CR_DEBUG=0>)
+	target_compile_definitions(${target} PRIVATE $<$<CONFIG:RelWithDebInfo>:CR_RELEASE=1>)
+	target_compile_definitions(${target} PRIVATE $<$<NOT:$<CONFIG:RelWithDebInfo>>:CR_RELEASE=0>)
+	target_compile_definitions(${target} PRIVATE $<$<CONFIG:Profile>:CR_PROFILE=1>)
+	target_compile_definitions(${target} PRIVATE $<$<NOT:$<CONFIG:Profile>>:CR_PROFILE=0>)
+	target_compile_definitions(${target} PRIVATE $<$<CONFIG:Final>:CR_FINAL=1>)
+	target_compile_definitions(${target} PRIVATE $<$<NOT:$<CONFIG:Final>>:CR_FINAL=0>)
+	target_compile_definitions(${target} PRIVATE $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<OR:$<CONFIG:Profile>,$<CONFIG:Final>>>:NDEBUG>)
+	
+	# generator expressions aren't working with INTERPROCEDURAL_OPTIMIZATION_PROFILE
+	set_target_properties(${target} PROPERTIES INTERPROCEDURAL_OPTIMIZATION_PROFILE TRUE)
+	set_target_properties(${target} PROPERTIES INTERPROCEDURAL_OPTIMIZATION_FINAL TRUE)	
+	
+	target_link_options(${target} PRIVATE $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>>:/Debug:fastlink>)
+endfunction()
+
+
+function(settings3rdParty target)	
+	addCommon(${target})
+	
+	source_group("Public Interface" FILES ${PUBLIC_INTERFACE})
+	source_group("Source" FILES ${SRCS})
+	source_group("Build" FILES ${BUILD})
+	
+	target_compile_options(${target} PRIVATE /W0)
+	target_compile_options(${target} PRIVATE /WX-)
+	
+	set_property(TARGET ${target} APPEND PROPERTY FOLDER 3rdParty)
+endfunction()
+
+function(settingsCR target)	
+	addCommon(${target})
+	
+	source_group(TREE ${root} FILES ${PUBLIC_INTERFACE})
+	source_group(TREE ${root} FILES ${SRCS})
+	source_group(TREE ${root} FILES ${BUILD})
+		
+	target_compile_options(${target} PRIVATE /W4)
+	target_compile_options(${target} PRIVATE /WX)
+	
+	target_include_directories(${target} PRIVATE "${CMAKE_CURRENT_LIST_DIR}/../../3rdParty/include")
+	
+	# disable unit tests in profile and final builds
+	# target_compile_definitions(${target} PRIVATE $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<OR:$<CONFIG:Profile>,$<CONFIG:Final>>>:DOCTEST_CONFIG_DISABLE>)
+	
+	set_target_properties(${target} PROPERTIES
+		VS_GLOBAL_RunCodeAnalysis false
+
+		# Use visual studio core guidelines
+		VS_GLOBAL_EnableMicrosoftCodeAnalysis false
+
+		# Use clangtidy
+		VS_GLOBAL_EnableClangTidyCodeAnalysis true
+		VS_GLOBAL_ClangTidyChecks "-checks=-*,modernize-*, -modernize-avoid-c-arrays, -modernize-use-trailing-return-type, \
+bugprone-*, -bugprone-bool-pointer-implicit-conversion, cppcoreguidelines-*, -cppcoreguidelines-avoid-c-arrays, -cppcoreguidelines-pro-bounds-constant-array-index, misc-*, performance-*, readability-*, -readability-uppercase-literal-suffix"
+	)
+endfunction()
