3db0fa969fb19f27974d37eb784f425989bd1a20
duhone <duhone@outlook.com>
2022-04-03 08:56:07 -0700

    add secret rabit code 3rd party lib

---
+++ 3rdParty/libsamplerate/build/build.cmake
@@ -0,0 +1,29 @@
+set(root "${CMAKE_CURRENT_LIST_DIR}/..")
+
+set(INTERFACE_FILES
+    ${root}/libsamplerate/include/samplerate.h
+)
+
+set(SOURCE_FILES
+    ${root}/libsamplerate/src/common.h
+    ${root}/libsamplerate/src/fastest_coeffs.h
+    ${root}/libsamplerate/src/high_qual_coeffs.h
+    ${root}/libsamplerate/src/mid_qual_coeffs.h
+    ${root}/libsamplerate/src/samplerate.c
+    ${root}/libsamplerate/src/src_linear.c
+    ${root}/libsamplerate/src/src_sinc.c
+    ${root}/libsamplerate/src/src_zoh.c
+)
+
+set(BUILD_FILES
+    ${root}/build/build.cmake
+)
+
+add_library(samplerate OBJECT 
+	${INTERFACE_FILES} 
+	${SOURCE_FILES} 
+	${BUILD_FILES}
+)
+settings3rdParty(samplerate)
+
+target_include_directories(samplerate SYSTEM PUBLIC "${root}/libsamplerate/include")
