03f70ecbd2a6a702051c5c6ce90f146c91012062
Eric Duhon <duhone@outlook.com>
2023-01-15 12:39:01 -0800

    [core] add iteration to bitset

--- CR/Engine/Core/tests/BitSet.cpp
+++ CR/Engine/Core/tests/BitSet.cpp
@@ -2,6 +2,8 @@ import CR.Engine.Core.BitSet;
 
 #include <doctest/doctest.h>
 
+import <vector>;
+
 using namespace std;
 using namespace CR::Engine::Core;
 
@@ -48,4 +50,39 @@ TEST_CASE("BitSet Find integer not in set") {
 	CHECK(bitset.FindNotInSet() == 128);
 	bitset.insert(128);
 	CHECK(bitset.FindNotInSet() == 129);
+}
+
+TEST_CASE("BitSet iterate") {
+	BitSet<512> bitset;
+	// check can iterate an empty bitset, loop should exit immediatly
+	CHECK(bitset.empty() == true);
+	for([[maybe_unused]] const auto val : bitset) { CHECK(false); }
+
+	// check can iterate an a bitset with one value
+	bitset.insert(6);
+	CHECK(bitset.size() == 1);
+	int counter = 0;
+	std::vector<std::uint16_t> expected;
+	expected.push_back(6);
+	for(const auto val : bitset) {
+		CHECK(counter < expected.size());
+		CHECK(val == expected[counter]);
+		++counter;
+	}
+
+	// check can iterate an a bitset with several values
+	bitset.insert(0);
+	bitset.insert(128);
+	bitset.insert(312);
+	expected.clear();
+	expected.push_back(0);
+	expected.push_back(6);
+	expected.push_back(128);
+	expected.push_back(312);
+	counter = 0;
+	for(const auto val : bitset) {
+		CHECK(counter < expected.size());
+		CHECK(val == expected[counter]);
+		++counter;
+	}
 }
