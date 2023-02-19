30fe2811b5347b8ee76616362621b85bc7b8f116
Eric Duhon <duhone@outlook.com>
2023-02-19 14:10:21 -0800

    [3rdParty] Add Volk

---
+++ 3rdParty/volk/build/build.cmake
@@ -0,0 +1,31 @@
+set(root "${CMAKE_CURRENT_LIST_DIR}/..")
+
+find_package(Vulkan REQUIRED)
+
+set(INTERFACE_FILES
+    ${root}/volk/volk.h
+)
+
+set(SOURCE_FILES
+    ${root}/volk/volk.c
+)
+
+set(BUILD_FILES
+    ${root}/build/build.cmake
+)
+
+add_library(volk OBJECT 
+	${INTERFACE_FILES} 
+	${SOURCE_FILES} 
+	${BUILD_FILES}
+)
+settings3rdParty(volk)
+
+target_compile_definitions(volk PUBLIC VK_USE_PLATFORM_WIN32_KHR)
+target_compile_definitions(volk PUBLIC VK_NO_PROTOTYPES)
+
+target_include_directories(volk SYSTEM PUBLIC "${root}/volk")
+
+target_link_libraries(volk PUBLIC
+	Vulkan::Headers
+)
